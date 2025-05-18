aae36cfbe96ea78595ec7cc5c558c5fc0b4e8060
shabnum-ara <111237187+shabnum-ara@users.noreply.github.com>
2025-05-18 13:10:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,16 +11,16 @@ import orderRouter from "./routes/orderRoute.js";
 //  app config
 dotenv.config();
 
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-const app = express();
-const port = process.env.PORT || 4000;
-connectDB();
-connectCloudinary();
 
 // middlewares
 app.use(express.json());
