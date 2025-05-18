a0583960b8add1a0d4526d83b4ac5f34224756b0
shabnum-ara <111237187+shabnum-ara@users.noreply.github.com>
2025-05-18 13:56:38 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
 import { ToastContainer, toast } from "react-toastify";
 import Verify from "./pages/Verify";
+import ScrollTop from "./components/ScrollTop";
 
 function App() {
   return (
